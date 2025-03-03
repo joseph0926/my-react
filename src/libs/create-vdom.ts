6f91cbe85c56804ae9a5832b1fe5621ad14f2da2
joseph0926 <joseph0926@naver.com>
2025-03-03 09:46:30 +0900

    [refactor] 주석 노드등 처리 강화

--- src/libs/create-vdom.ts
+++ src/libs/create-vdom.ts
@@ -7,38 +7,40 @@ import type { VNodeType, VNode, ElementType } from "@/types/dom.type";
  * @returns vDom
  */
 export function createVNodeFromDOM(node: Node): VNode<VNodeType> {
-  // node가 text node인지 확인
-  if (node.nodeType === Node.TEXT_NODE) {
-    const nodeValue = node.textContent || "";
-
-    // 텍스트가 존재하지 않으면 null 반환
-    // 단, 의도한 공백은 존재할수있으므로 공백 자체 제거는 x
-    if (nodeValue.trim().length === 0) {
-      return null;
+  switch (node.nodeType) {
+    case Node.TEXT_NODE: {
+      const nodeValue = node.textContent || "";
+      if (nodeValue.trim().length === 0) {
+        return null;
+      }
+      return {
+        type: "TEXT_ELEMENT",
+        props: { nodeValue },
+        children: null,
+      };
     }
 
-    return {
-      type: "TEXT_ELEMENT",
-      props: { nodeValue },
-      children: null,
-    };
+    case Node.ELEMENT_NODE: {
+      const element = node as HTMLElement;
+      const type = element.tagName.toLowerCase() as ElementType;
+      let props: Record<string, string> = {};
+
+      for (const attr of Array.from(element.attributes)) {
+        props[attr.name] = attr.value;
+      }
+
+      const childrenNodes = Array.from(element.childNodes)
+        .map(createVNodeFromDOM)
+        .filter((child): child is VNode<any> => child !== null);
+
+      return {
+        type,
+        props,
+        children: childrenNodes.length > 0 ? childrenNodes : null,
+      };
+    }
+
+    default:
+      return null;
   }
-
-  const element = node as HTMLElement;
-  const type = element.tagName.toLowerCase() as ElementType;
-  let props: Record<string, string> = {};
-
-  for (const attr of Array.from(element.attributes)) {
-    props[attr.name] = attr.value;
-  }
-
-  const childrenNodes = Array.from(element.childNodes)
-    .map(createVNodeFromDOM)
-    .filter((child): child is VNode<any> => child !== null);
-
-  return {
-    type,
-    props,
-    children: childrenNodes.length > 0 ? childrenNodes : null,
-  };
 }
