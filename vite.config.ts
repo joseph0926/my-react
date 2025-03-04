5332543248fbdc82ccefb32e15efc71c394a7042
joseph0926 <joseph0926@naver.com>
2025-03-04 19:49:58 +0900

    [chore] babel 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import path from "path";
 import { defineConfig } from "vite";
+import babelPlugin from "vite-plugin-babel";
 
 export default defineConfig({
+  esbuild: {
+    jsx: "preserve",
+  },
+  plugins: [
+    babelPlugin({
+      babelConfig: {
+        presets: [
+          ["@babel/preset-env", { targets: "defaults" }],
+          [
+            "@babel/preset-react",
+            { pragma: "createElement", pragmaFrag: "Fragment" },
+          ],
+          "@babel/preset-typescript",
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
