94c51d6f7c4c36e207e906118faa5355edad9c4b
joseph0926 <joseph0926@naver.com>
2025-03-08 09:25:28 +0900

    [refactor] react 타입 개선 - 내부 프로퍼티 _store에 대한 처리 추가

--- src/types/react.type.ts
+++ src/types/react.type.ts
@@ -1,9 +1,24 @@
 /**
+ * React 내부에서만 사용하는 프로퍼티를 나타냅니다.
+ */
+interface ReactElementStore {
+  validated?: number;
+  [key: string]: any;
+}
+
+/**
  * 사용자가 정의한 함수형 컴포넌트를 나타냅니다.
  *
  * @template P 컴포넌트가 받을 수 있는 props의 타입입니다.
  */
 type JSXElementConstructor<P> = (props: P) => ReactNode;
+
+/**
+ * ReactElement의 type에 대한 타입입니다.
+ */
+export type ReactElementType =
+  | keyof HTMLElementTagNameMap
+  | JSXElementConstructor<any>;
 
 /**
  * JSX 요소를 나타냅니다.
@@ -15,15 +30,16 @@ type JSXElementConstructor<P> = (props: P) => ReactNode;
  * @template T 컴포넌트 또는 태그의 타입
  *
  */
-interface ReactElement<
+export interface ReactElement<
   P = any,
-  T extends keyof HTMLElementTagNameMap | JSXElementConstructor<any> =
+  T extends ReactElementType =
     | keyof HTMLElementTagNameMap
     | JSXElementConstructor<any>
 > {
   type: T;
   props: P;
   key: string | null;
+  _store?: ReactElementStore;
 }
 
 /**
@@ -32,7 +48,7 @@ interface ReactElement<
  * {@link ReactElement}가 JSX 요소만 나타내는 반면,
  * `ReactNode`는 렌더링 가능한 모든 요소를 나타냅니다.
  */
-type ReactNode =
+export type ReactNode =
   | ReactElement
   | string
   | number
