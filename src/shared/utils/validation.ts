a93254d31fdcd1801692c34a42b8e235d435e846
joseph0926 <joseph0926@naver.com>
2025-03-08 09:25:58 +0900

    [refactor] key validation 타입 강화

--- src/shared/utils/validation.ts
+++ src/shared/utils/validation.ts
@@ -1,4 +1,6 @@
+import { ReactElement, ReactElementType, ReactNode } from "@/types/react.type";
 import { __DEV__ } from "../constants";
+import { REACT_ELEMENT_TYPE } from "../symbol/react.symbol";
 
 /**
  * Ensure that every element either is passed in a static location, in an
@@ -9,7 +11,10 @@ import { __DEV__ } from "../constants";
  * @param {ReactNode} node 자식 React 엘리먼트
  * @param {*} parentType 자식 React 엘리먼트의 부모 React 엘리먼트 태그
  */
-export function validateChildKeys(node, parentType) {
+export function validateChildKeys(
+  node: ReactNode,
+  _parentType: ReactElementType
+) {
   if (__DEV__) {
     // With owner stacks is, no warnings happens. All we do is
     // mark elements as being in a valid static child position so they
@@ -28,7 +33,7 @@ export function validateChildKeys(node, parentType) {
  * @param {?object} object
  * @return {boolean} object가 React 엘리먼트라면 true 반환
  */
-export function isValidElement(object) {
+export function isValidElement(object: any): object is ReactElement {
   return (
     typeof object === "object" &&
     object !== null &&
