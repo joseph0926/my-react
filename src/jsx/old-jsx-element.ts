71ca861b2c51b9e758a6f1a01d6f6280c2393a4a
joseph0926 <joseph0926@naver.com>
2025-03-09 18:00:05 +0900

    [refactor] v17 이전 createElement 함수 개선

--- src/jsx/old-jsx-element.ts
+++ src/jsx/old-jsx-element.ts
@@ -1,70 +1,60 @@
 // 해당 파일은 react 17이전에 사용하던 JSX 생성방식입니다.
 
+import { ReactElement } from "@/libs/react-element";
 import { __DEV__ } from "@/shared/constants";
-import { validateChildKeys } from "@/shared/utils/validation";
-import { ReactElementType, ReactNode } from "@/types/react.type";
+import {
+  checkKeyStringCoercion,
+  hasValidKey,
+  validateChildKeys,
+} from "@/shared/utils/validation";
+import type { Key, ReactElementType, ReactNode } from "@/types/common";
+import type { Config } from "@/types/element.interface";
 
+/**
+ * createElement
+ * @description react v17 이전에 react element를 만드는 함수입니다.
+ * @param type
+ * @param config
+ * @param children
+ * @returns `ReactElement`
+ */
 export function createElement(
   type: ReactElementType,
-  config,
+  config: Config | null,
   children: ReactNode
 ) {
-  if (__DEV__) {
-    // arguments[2]부터는 자식 컴포넌트 (1 => type, 2 => config)
-    // 따라서 자식 요소를 모두 순회하면서 key 검증을 수행함
-    for (let i = 2; i < arguments.length; i++) {
-      validateChildKeys(arguments[i], type);
-    }
+  // arguments[2]부터는 자식 컴포넌트 (0 => type, 1 => config)
+  // 따라서 자식 요소를 모두 순회하면서 key 검증을 수행함
+  for (let i = 2; i < arguments.length; i++) {
+    // 각 자식 컴포넌트의 `_store`에 검증됨을 명시
+    validateChildKeys(arguments[i], type);
   }
 
-  let propName;
-  const props = {};
+  let propName: string;
+  let key: Key = null;
+  const props: { [propName: string]: any } = {};
 
-  let key = null;
-
-  if (config != null) {
-    if (__DEV__) {
-      if (
-        !didWarnAboutOldJSXRuntime &&
-        "__self" in config &&
-        !("key" in config)
-      ) {
-        didWarnAboutOldJSXRuntime = true;
-        console.warn(
-          "Your app (or one of its dependencies) is using an outdated JSX " +
-            "transform. Update to the modern JSX transform for " +
-            "faster performance: https://react.dev/link/new-jsx-transform"
-        );
-      }
-    }
-
+  // config가 존재하면
+  if (config !== null) {
+    // config 객체에 올바른 key가 설정되어 있는지 검사
     if (hasValidKey(config)) {
-      if (__DEV__) {
-        checkKeyStringCoercion(config.key);
-      }
-      key = "" + config.key;
+      key = checkKeyStringCoercion(config.key);
     }
 
-    // Remaining properties are added to a new props object
+    // 속성들중에 예약를 제외하고 props 객체로 할당
     for (propName in config) {
-      if (
-        hasOwnProperty.call(config, propName) &&
-        // Skip over reserved prop names
-        propName !== "key" &&
-        // Even though we don't use these anymore in the runtime, we don't want
-        // them to appear as props, so in createElement we filter them out.
-        // We don't have to do this in the jsx() runtime because the jsx()
-        // transform never passed these as props; it used separate arguments.
-        propName !== "__self" &&
-        propName !== "__source"
-      ) {
+      if (Object.hasOwn(config, propName) && propName !== "key") {
         props[propName] = config[propName];
       }
     }
   }
 
-  // Children can be more than one argument, and those are transferred onto
-  // the newly allocated props object.
+  /*
+    JSX를 통해 전달된 children가 몇 개인지에 따라 다르게 처리
+    - 없으면 → props.children이 x
+    - 1개면 → 자식을 그대로 할당 (props.children = child)
+    - 2개 이상이면 → 배열로 묶어서 처리 (props.children = [child1, child2, ...])
+  */
   const childrenLength = arguments.length - 2;
   if (childrenLength === 1) {
     props.children = children;
@@ -73,32 +63,23 @@ export function createElement(
     for (let i = 0; i < childrenLength; i++) {
       childArray[i] = arguments[i + 2];
     }
-    if (__DEV__) {
-      if (Object.freeze) {
-        Object.freeze(childArray);
-      }
+    // 실수나 의도적으로 chidlren[]을 조작하지 못하게 처리
+    if (Object.freeze) {
+      Object.freeze(childArray);
     }
     props.children = childArray;
   }
 
-  // Resolve default props
-  if (type && type.defaultProps) {
-    const defaultProps = type.defaultProps;
-    for (propName in defaultProps) {
-      if (props[propName] === undefined) {
-        props[propName] = defaultProps[propName];
-      }
-    }
-  }
-  if (__DEV__) {
-    if (key) {
-      const displayName =
-        typeof type === "function"
-          ? type.displayName || type.name || "Unknown"
-          : type;
-      defineKeyPropWarningGetter(props, displayName);
-    }
-  }
+  // 컴포넌트에 정의된 기본 속성값(defaultProps) 을 props에 적용
+  // 다만 최신에 와서는 defaultProps는 처리 안하므로 여기선 무시
+  // if (type && type.defaultProps) {
+  //   const defaultProps = type.defaultProps;
+  //   for (propName in defaultProps) {
+  //     if (props[propName] === undefined) {
+  //       props[propName] = defaultProps[propName];
+  //     }
+  //   }
+  // }
 
   return ReactElement(
     type,
--- src/shared/utils/validation.ts
+++ src/shared/utils/validation.ts
@@ -1,6 +1,8 @@
-import { ReactElement, ReactElementType, ReactNode } from "@/types/react.type";
 import { __DEV__ } from "../constants";
 import { REACT_ELEMENT_TYPE } from "../symbol/react.symbol";
+import type { ReactNode } from "@/types/react.type";
+import type { ReactElementType } from "@/types/common";
+import type { Config, ReactElement } from "@/types/element.interface";
 
 /**
  * validateChildKeys
@@ -14,9 +16,11 @@ export function validateChildKeys(
   _parentType: ReactElementType
 ) {
   if (__DEV__) {
-    // default가 validated된 상태 - 동적인 엘리먼트가 아니라면 key가 없어도 무방
+    // 인자로 받은 자식 element가 React Element인지 확인
     if (isValidElement(node)) {
+      // React Element에 내부 디버깅용 `_store`가 존재하는지 확인
       if (node._store) {
+        // 기본으로 `_store`에 검증됨을 명시
         node._store.validated = 1;
       }
     }
@@ -35,4 +39,53 @@ export function isValidElement(object: any): object is ReactElement {
     object !== null &&
     object.$$typeof === REACT_ELEMENT_TYPE
   );
+}
+
+/**
+ * hasValidKey
+ * @description config 객체에 올바른 key가 설정되어 있는지 검사하는 함수
+ * @param config
+ * @returns `true` | `false`
+ */
+export function hasValidKey(config: Config) {
+  // key가 undefined면 false, 값이 있으면 true
+  return config.key !== undefined;
+}
+
+/**
+ * checkKeyStringCoercion
+ * @description key로 전달된 값이 문자열로 강제 변환(string coercion) 가능한 값인지 검사하는 함수
+ * @param keyValue key 속성에 전달된 값
+ * @returns
+ * @example 1 => "1" 가능 / {{}} => "{{}}" 불가능
+ */
+export function checkKeyStringCoercion(keyValue: unknown) {
+  const type = typeof keyValue;
+
+  if (keyValue === null || keyValue === undefined) {
+    console.error(`key 값으로 null 또는 undefined는 허용되지 않습니다.`);
+    throw new Error("Invalid key type");
+  }
+
+  // 객체 타입 체크
+  if (type === "object") {
+    const displayType = Array.isArray(keyValue) ? "Array" : "Object";
+    console.error(`key 값으로 ${displayType} 타입은 허용되지 않습니다.`);
+    throw new Error(`Invalid key type: ${displayType}`);
+  }
+
+  // symbol, function 체크
+  if (type === "symbol" || type === "function") {
+    console.error(`key 값으로 ${type} 타입은 허용되지 않습니다.`);
+    throw new Error(`Invalid key type: ${type}`);
+  }
+
+  try {
+    // keyValue를 문자열로 변환 시도
+    return "" + keyValue;
+  } catch (error) {
+    // 실패시 에러 던짐
+    console.error(`key 값을 문자열로 변환할 수 없습니다:`, error);
+    throw error;
+  }
 }
