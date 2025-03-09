d868ef7a7bcd86d3e4a7f2103b353c59c0bfafdd
김영훈 <joseph0926@naver.com>
2025-03-10 08:43:52 +0900

    [refactor] 커스텀 createElement 적용을 위한 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
-import babelPlugin from "vite-plugin-babel";
 
 export default defineConfig({
   esbuild: {
-    jsx: "preserve",
+    jsxFactory: "createElement",
+    jsxFragment: "Fragment",
   },
-  plugins: [
-    babelPlugin({
-      babelConfig: {
-        presets: [
-          ["@babel/preset-env", { targets: "defaults" }],
-          [
-            "@babel/preset-react",
-            { pragma: "createElement", pragmaFrag: "Fragment" },
-          ],
-          "@babel/preset-typescript",
-        ],
-      },
-    }),
-  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
